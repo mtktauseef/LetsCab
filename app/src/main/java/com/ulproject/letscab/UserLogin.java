7450c653db548e34548460726f34ecd732bd64b0
mtktauseef <mtktauseef@gmail.com>
2019-11-16 13:49:40 +0000

    Intial Update

---
+++ app/src/main/java/com/ulproject/letscab/UserLogin.java
@@ -0,0 +1,14 @@
+package com.ulproject.letscab;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserLogin extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_login);
+    }
+
+}
